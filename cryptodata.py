129dcdba9dd8aee1bd85efd2dcf998095a4a2235
Breno Rodrigues Brito <brenorb@gmail.com>
2018-07-26 21:44:23 -0300

    organization only

--- cryptodata.py
+++ cryptodata.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import json
 import requests
-import json
 import time
 
 
@@ -34,44 +33,32 @@ def get_pairs(api = "cryptowatch", active = True):
 
 def get_ohlc(market, pair, period = 1440, api = "cryptowatch", start = "", end = "", local_timezone = True):
     """get OHLC prices available in API"""
+    
     period *= 60
     period = str(period)
     params = []
+    dates = ["",""]
     url = "https://api.cryptowat.ch/markets/" + market + "/" + pair + "/ohlc"
-    print(url)
-    if (end == ""):
-        before = ""
-    else:
-        before = time.mktime(time.strptime(end, "%d %m %y %H %M"))
-    if (start == ""):
-        after = start
-    else:
-        after = time.mktime(time.strptime(start, "%d %m %y %H %M"))
-    if (not before or not after):
-        prms = [('before', before), ('after', after)]
-    else:
-        prms = {'before': int(before) , 'after': int(after) }
-
-#    print('params',prms)
+    
+    for i, date in enumerate([start, end]):
+        if (not date):
+            dates[i] = date
+        else:
+            try:
+                dates[i] = int(time.mktime(time.strptime(date, "%d %m %y %H %M"))) #+ local_timezone*time.timezone
+            except:
+                print("Error: Datetime format d m y H M")
+    
+    prms = {'before': dates[1], 'after': dates[0]}
     
     if api == "cryptowatch":
         try:
             r = requests.get(url, params=prms).json()['result'][period]
-            #ohlc = [item['pair'] for item in r]
-            if (local_timezone):            
-                a = pd.DataFrame(r, columns = ['date(local)','open','high','low','close','volume','neg'])
-                a[a.columns[0]] = pd.to_datetime(a[a.columns[0]] - time.timezone, unit = 's')
-                return a
-            else:
-                a = pd.DataFrame(r, columns = ['date(utc)','open','high','low','close','volume','neg'])
-                a[a.columns[0]] = pd.to_datetime(a[a.columns[0]], unit = 's')
-                return a               
-                
-
+            a = pd.DataFrame(r, columns = ['date(local)','open','high','low','close','volume','neg'])
+            a[a.columns[0]] = pd.to_datetime(a[a.columns[0]] - local_timezone*time.timezone, unit = 's')
+            return a        
         except:
             print("Error: disfunctional API from Cryptowatch")
             
-#columns = ['period','open','high','low','close','volume','neg']    
+#columns = ['period','open','high','low','close','volume','neg']  
 
-
-    
